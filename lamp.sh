3d8f6323b197ff98df8a0cb13799fdd3a31fc1a3
nikhilnagesh <nikhil81296@gmail.com>
2020-06-07 09:17:03 +0000

    lamp-2023

--- lamp.sh
+++ lamp.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 Script for lamp setup
-sudo yum -y install mysql-5.6
-sudo yum -y install php70-w
-sudo yum -y install httpd-2.2
+sudo yum -y install mysql-8.0
+sudo yum -y install php90-w
+sudo yum -y install httpd-2.7
 
